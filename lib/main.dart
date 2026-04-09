4d2e16475f4299176f3612a81e101f56c2fd78b8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 20:54:29 -0300

    refact: atualizar cor theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ironkey/app_theme.dart';
+import 'package:ironkey/password_generator.dart';
+import 'package:ironkey/pin_password_generator.dart';
+import 'package:ironkey/standard_password_generator.dart';
 
 void main() {
   runApp(IronKeyApp());
@@ -17,7 +20,7 @@ class IronKeyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       title: "IronKey",
       home: IronKeyScreen(),
     );
@@ -32,18 +35,22 @@ class IronKeyScreen extends StatefulWidget {
 }
 
 class _IronKeyScreenState extends State<IronKeyScreen> {
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passWordController = TextEditingController();
+
+  PassowardType passowardSelectType = PassowardType.pin;
+  bool isEditable = false;
+
   @override
   void initState() {
     super.initState();
-    _passwordController.addListener(() {
+    _passWordController.addListener(() {
       setState(() {});
     });
   }
 
   @override
   void dispose() {
-    _passwordController.dispose();
+    _passWordController.dispose();
     super.dispose();
   }
 
@@ -55,26 +62,31 @@ class _IronKeyScreenState extends State<IronKeyScreen> {
   }
 
   void generatePassword() {
-    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lower = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = "!@#\$%&*";
-    final chars = upper + lower + numbers + symbols;
-    final random = Random();
+    late final PasswordGenerator generator;
+
+    switch (passowardSelectType) {
+      case PassowardType.pin:
+        generator = PinPasswordGenerator();
+        break;
+      case PassowardType.standard:
+        generator = StandardPasswordGenerator();
+        break;
+    }
+
     setState(() {
-      _passwordController.text = List.generate(
-        12,
-        (_) => chars[random.nextInt(chars.length)],
-      ).join();
+      _passWordController.text = generator.generate(8);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final ColorScheme = theme.colorScheme;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               Expanded(
@@ -92,38 +104,97 @@ class _IronKeyScreenState extends State<IronKeyScreen> {
                     ),
                     SizedBox(height: 16),
                     Text(
-                      "Jarvis, digite uma senha segura",
+                      "Sexta-feira, proteja a minha senha!",
                       textAlign: TextAlign.center,
                       maxLines: 2,
                       overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24),
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 24,
+                      ),
                     ),
                     SizedBox(height: 16),
                     TextField(
-                      controller: _passwordController,
+                      enabled: isEditable,
+                      controller: _passWordController,
                       maxLength: 12,
                       decoration: InputDecoration(
-                        labelText: "Password",
+                        labelText: "Passoward",
                         border: OutlineInputBorder(),
                         prefix: Icon(Icons.lock),
-                        suffix: _passwordController.text.isNotEmpty
+                        suffix: _passWordController.text.isNotEmpty
                             ? IconButton(
                                 onPressed: () {
-                                  copyPassword(_passwordController.text);
+                                  copyPassword(_passWordController.text);
                                 },
                                 icon: Icon(Icons.copy),
                               )
                             : null,
                       ),
                     ),
+
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text("Tipo de senha"),
+                    ),
+
+                    Row(
+                      children: [
+                        Expanded(
+                          child: RadioListTile(
+                            value: PassowardType.pin,
+                            groupValue: passowardSelectType,
+                            onChanged: (value) {
+                              setState(() {
+                                passowardSelectType = value!;
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RadioListTile(
+                            value: PassowardType.standard,
+                            groupValue: passowardSelectType,
+                            title: Text("Senha padrão"),
+                            onChanged: (value) {
+                              setState(() {
+                                passowardSelectType = value!;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                    Divider(color: ColorScheme.outline),
+
+                    Row(
+                      children: [
+                        Icon(isEditable ? Icons.lock_open : Icons.lock),
+                        SizedBox(width: 8),
+                        Expanded(child: Text("Permitir editar a senha?")),
+                        Switch(
+                          value: isEditable,
+                          onChanged: (value) {
+                            setState(() {
+                              isEditable = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
-          
+              Divider(color: ColorScheme.outline),
+              const SizedBox(height: 20,),
+
+              if(isEditable) Text("Senha customizada"),
               SizedBox(
                 width: double.infinity,
-                child: FilledButton(onPressed: generatePassword, 
-                child: Text("Gerar senha")),
+                child: FilledButton(
+                  onPressed: generatePassword,
+                  child: Text("Gerar senha"),
+                ),
               ),
             ],
           ),
@@ -132,3 +203,5 @@ class _IronKeyScreenState extends State<IronKeyScreen> {
     );
   }
 }
+
+enum PassowardType { pin, standard }
